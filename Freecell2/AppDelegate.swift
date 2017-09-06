e4fdb95752806c5af0695b450149b90343737ccd
Gary Kerr <garykerrwork@gmail.com>
2017-09-06 01:10:47 +0100

    Tidy

--- Freecell2/AppDelegate.swift
+++ Freecell2/AppDelegate.swift
@@ -12,7 +12,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -20,6 +19,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-    
-    
 }
